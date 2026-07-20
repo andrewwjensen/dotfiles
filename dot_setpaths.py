db464cf5e4f479b724b9fed098bf29c8f5e1e1d6
Andrew Jensen <andy.jensen@trane.com>
2026-07-20 08:44:57 -0500

    Add Beyond Compare prefs; update iTerm2 prefs and setpaths.py

--- dot_setpaths.py
+++ dot_setpaths.py
@@ -5,11 +5,8 @@
 import os
 import sys
 from collections import deque
-from pathlib import Path
 
 debug = False
-
-HOME = Path(os.environ['HOME'])
 
 
 def canon(directory):
@@ -17,7 +14,7 @@ def canon(directory):
     if not directory or not os.path.isdir(directory):
         return None
     else:
-        return str(directory)
+        return directory
 
 
 def parse_path_variable(var_name) -> deque[str]:
@@ -54,22 +51,22 @@ def set_path():
     add_path(paths, '/usr/local/bin', at_front=True)
     add_path(paths, '/opt/homebrew/bin', at_front=True)
     add_path(paths, '/usr/local/opt/findutils/libexec/gnubin', at_front=True)
-    add_path(paths, HOME / 'Library/Python/3.7/bin', at_front=True)
-    add_path(paths, HOME / '.local/bin', at_front=True)
-    add_path(paths, HOME / 'IdeaProjects/stratus-aws/scripts', at_front=True)
-    add_path(paths, HOME / 'bin/archind', at_front=True)
-    add_path(paths, HOME / 'bin', at_front=True)
-    add_path(paths, HOME / 'unix-environment/bin/archind', at_front=True)
-    add_path(paths, HOME / 'unix-environment/bin/src/mine/build-default', at_front=True)
-    add_path(paths, HOME / 'unix-environment/bin/src/mine/bazel-bin', at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], 'Library/Python/3.7/bin'), at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], '.local/bin'), at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], 'IdeaProjects/stratus-aws/scripts'), at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], 'bin/archind'), at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], 'bin'), at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], 'unix-environment/bin/archind'), at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], 'unix-environment/bin/src/mine/build-default'), at_front=True)
+    add_path(paths, os.path.join(os.environ['HOME'], 'unix-environment/bin/src/mine/bazel-bin'), at_front=True)
 
     # Other paths
-    add_path(paths, HOME / 'Library/Application Support/JetBrains/Toolbox/scripts')
-    add_path(paths, HOME / 'dev/util/tis')
-    add_path(paths, HOME / 'dev/util/bin')
-    add_path(paths, HOME / 'dev/scripts')
-    add_path(paths, HOME / 'dev/curlstuff')
-    add_path(paths, HOME / '.pyenv/bin')
+    add_path(paths, os.path.join(os.environ['HOME'], 'Library/Application Support/JetBrains/Toolbox/scripts'))
+    add_path(paths, os.path.join(os.environ['HOME'], 'dev/util/tis'))
+    add_path(paths, os.path.join(os.environ['HOME'], 'dev/util/bin'))
+    add_path(paths, os.path.join(os.environ['HOME'], 'dev/scripts'))
+    add_path(paths, os.path.join(os.environ['HOME'], 'dev/curlstuff'))
+    add_path(paths, os.path.join(os.environ['HOME'], '.pyenv/bin'))
     add_path(paths, '/usr/local/sbin')
     add_path(paths, '/Library/Frameworks/Python.framework/Versions/3.7/bin')
     add_path(paths, '/usr/local/opt/tomcat@8/bin')
@@ -86,16 +83,15 @@ def set_path():
 def set_pythonpath():
     """Set up the PYTHONPATH environment variable"""
     paths = parse_path_variable('PYTHONPATH')
-    add_path(paths, HOME / 'dev/util')
+    add_path(paths, os.path.join(os.environ['HOME'], 'dev/util'))
     set_path_from_list('PYTHONPATH', paths)
 
 
 def main():
-    global HOME
-    if HOME == Path('/'):
+    if os.environ['HOME'] == '/':
         # Special case for root user; just set HOME to something
         # bogus so we don't get an unwanted entry for /bin
-        HOME = Path('/InVaLiD:dIrEcToRy')
+        os.environ['HOME'] = '/InVaLiD:dIrEcToRy'
     set_path()
     set_pythonpath()
     return 0
